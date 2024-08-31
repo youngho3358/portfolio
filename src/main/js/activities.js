42b2186e1f237fe828997cf5e050ad749de683cb
youngho3358 <youngho3358@gmail.com>
2024-08-31 00:46:45 -0700

    자기소개, 프로젝트, 감사합니다 수정 필요

--- src/main/js/activities.js
+++ src/main/js/activities.js
@@ -2,62 +2,6 @@ import { Navigate, useNavigate } from 'react-router-dom';
 import './../css/activities.css';
 import { forwardRef } from 'react';
 
-// const Activities = forwardRef((props, ref) => {
-
-//     return (
-//         <>
-//             <div ref={ref} style={{ background: 'rgba(107, 142, 35, 0.8)', height: '1500px' }}>
-//                 <div style={{ width: "500px", margin: "auto" }}>
-//                     <div style={{ textAlign: 'center', fontSize: '55px', paddingTop: '30px', borderBottom: '1px solid gray', width: '300px', margin: 'auto', fontWeight: "bold", height: "90px", color: "white" }}>ACTIVITIES</div>
-//                 </div>
-//                 <div style={{ display: "flex", margin: "auto", width: "1280px" }}>
-//                     <div style={{ textAlign: 'center', width: "1280px" }}>
-//                         <div style={{ backgroundColor: '#ffffff', width: '1280px', height: '1000px', boxShadow: '1rem 1rem 1rem 1rem rgb(68 68 68 / 20%)', borderRadius: '30px', marginTop: "50px", paddingTop:"50px" }}>
-//                             <div onClick={() => {window.location.href = 'http://www.kgitbank.kr/' }} className='activites_img' style={{ backgroundColor: 'white', width: '1100px', height: "150px", border: "1px solid rgba(0,0,0,0.1)", borderRadius: "15px", margin: "auto" }}>
-//                                 <div style={{display:"flex"}}>
-//                                     <div style={{textAlign:"left", marginLeft:"20px"}}>
-//                                         <img style={{width:'100px',height:'120px', marginTop:"10px"}} src={`${process.env.PUBLIC_URL}/img/activities/itbank.png`} alt='itbank'></img>
-//                                     </div>
-//                                     <div style={{fontSize:"25px", fontWeight:"bold", marginLeft:"30px", marginTop:"40px", textAlign:"left"}}>
-//                                         KG IT뱅크 부트 캠프 수료<br></br><label style={{fontWeight:"lighter"}}>2024.01.31 ~ 2024.08.02</label>
-//                                     </div>
-//                                 </div>
-//                             </div>
-
-//                             <div onClick={() => {window.location.href = 'https://codingapple.com/course/react-basic/' }} className='activites_img' style={{ backgroundColor: 'white', width: '1100px', height: "150px", border: "1px solid rgba(0,0,0,0.1)", borderRadius: "15px", margin: "30px auto auto auto" }}>
-//                                 <div style={{display:"flex"}}>
-//                                     <div style={{textAlign:"left", marginLeft:"20px"}}>
-//                                         <img style={{width:'100px',height:'120px', marginTop:"10px"}} src={`${process.env.PUBLIC_URL}/img/activities/코딩애플.png`} alt='itbank'></img>
-//                                     </div>
-//                                     <div style={{fontSize:"25px", fontWeight:"bold", marginLeft:"30px", marginTop:"40px", textAlign:"left"}}>
-//                                         코딩 애플 React 강의 수료<br></br>
-//                                         {/* <label style={{fontWeight:"lighter"}}>2024.01.31 ~ 2024.08.02</label> */}
-//                                     </div>
-//                                 </div>
-//                             </div>
-
-//                             <div onClick={() => {window.location.href = 'http://www.codingapple.com/' }} className='activites_img' style={{ backgroundColor: 'white', width: '1100px', height: "150px", border: "1px solid rgba(0,0,0,0.1)", borderRadius: "15px", margin: "30px auto auto auto" }}>
-//                                 <div style={{display:"flex"}}>
-//                                     <div style={{textAlign:"left", marginLeft:"20px"}}>
-//                                         <img style={{width:'100px',height:'120px', marginTop:"10px"}} src={`${process.env.PUBLIC_URL}/img/activities/코딩애플.png`} alt='itbank'></img>
-//                                     </div>
-//                                     <div style={{fontSize:"25px", fontWeight:"bold", marginLeft:"30px", marginTop:"40px", textAlign:"left"}}>
-//                                         코딩 애플 TypeScript 강의 수료<br></br>
-//                                         {/* <label style={{fontWeight:"lighter"}}>2024.01.31 ~ 2024.08.02</label> */}
-//                                     </div>
-//                                 </div>
-//                             </div>
-
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// });
-
-// export default Activities;
-
 const Activities = forwardRef((props, ref) => {
     return (
         <div ref={ref} className="activities-container">
@@ -70,23 +14,40 @@ const Activities = forwardRef((props, ref) => {
                         <img src={`${process.env.PUBLIC_URL}/img/activities/itbank.png`} alt='itbank' />
                     </div>
                     <div className="activities-description">
-                        KG IT뱅크 부트 캠프 수료<br /><label>2024.01.31 ~ 2024.08.02</label>
+                        KG IT뱅크 - 단과 과목 수료<br />
+                        <ul className='activities-list'>
+                            <li>IT기초</li>
+                            <li>네트워크 기초(CCNA)</li>
+                            <li>리눅스1</li>
+                            <li>리눅스2</li>
+                            <li>JAVA 프로그램 기초</li>
+                            <li>JAVA 프로그램 활용</li>
+                            <li>JSP</li>
+                            <li>Spring Framework</li>
+                        </ul>
                     </div>
                 </div>
-                <div className="activities-card" onClick={() => { window.location.href = 'https://codingapple.com/course/react-basic/' }}>
+                <div className="activities-card" onClick={() => { window.location.href = 'https://www.kgitbank.co.kr/jobtraining/sub.php?seq=20100' }}>
                     <div className="activities-img">
-                        <img src={`${process.env.PUBLIC_URL}/img/activities/코딩애플.png`} alt='coding apple react' />
+                        <img src={`${process.env.PUBLIC_URL}/img/activities/itbank.png`} alt='itbank' />
                     </div>
                     <div className="activities-description">
-                        코딩 애플 React 강의 수료
+                        KG IT뱅크 - 핀테크 서비스를 위한 풀스택 개발자 양성 과정 수료<br />
+                        <label>2024.01.31 ~ 2024.08.02</label>
                     </div>
                 </div>
-                <div className="activities-card" onClick={() => { window.location.href = 'https://codingapple.com/course/html-basics/' }}>
+                <div className="activities-card">
                     <div className="activities-img">
-                        <img src={`${process.env.PUBLIC_URL}/img/activities/코딩애플.png`} alt='coding apple typescript' />
+                        <img src={`${process.env.PUBLIC_URL}/img/activities/inflearn.png`} alt='coding apple react' />
                     </div>
                     <div className="activities-description">
-                        코딩 애플 HTML/CSS 강의 수료
+                        인프런 강의 수료<br/>
+                        <ul className='activities-list'>
+                            <a href='https://www.inflearn.com/course/ORM-JPA-Basic'><li>자바 ORM 표준 JPA 프로그래밍 - 기본편 (김영한) 수료</li></a>
+                            <a href='https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8-JPA-%ED%99%9C%EC%9A%A9-1'><li>실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 (김영한) 수료</li></a>
+                            <a href='https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%EB%8D%B0%EC%9D%B4%ED%84%B0-JPA-%EC%8B%A4%EC%A0%84'><li>실전! 스프링 데이터 JPA (김영한) 수료</li></a>
+                            <a href='https://www.inflearn.com/course/querydsl-%EC%8B%A4%EC%A0%84'><li>실전! Querydsl (김영한) 수료</li></a>
+                        </ul>
                     </div>
                 </div>
             </div>
--- src/main/js/project.js
+++ src/main/js/project.js
@@ -1,51 +1,6 @@
 import { forwardRef } from 'react';
 import './../css/project.css';
 
-// const Project = forwardRef((props, ref) => {
-//     return(
-//         <>
-//             <div ref={ref} style={{background:'#1d809f', height:'1500px'}}>
-//                 <div style={{width:"500px", margin:"auto"}}>
-//                     <div style={{textAlign:'center',fontSize:'55px',paddingTop:'30px',borderBottom:'1px solid gray',width:'300px', margin:'auto', fontWeight:"bold", height:"90px", color:"white"}}>PROJECTS</div>
-//                 </div>
-//                 <div style={{display:"flex", margin:"auto", width:"1280px"}}>
-//                     <div style={{textAlign:'center', width:"1280px"}}>
-
-                        
-//                         <div style={{backgroundColor:'#ffffff',width:'1280px',height:'550px',boxShadow:'1rem 1rem 1rem 1rem rgb(68 68 68 / 20%)',borderRadius:'30px', marginTop:"50px"}}>
-//                         <div style={{display:"flex"}}>
-//                                     <div style={{textAlign:"left", marginLeft:"20px",marginTop:'20px'}}>
-//                                         <img style={{width:'550px',height:'400px', marginTop:"50px",marginLeft:'60px',border:'2px solid black',borderRadius:'10px'}} src={`${process.env.PUBLIC_URL}/img/project/project1.png`} alt='itbank'></img>
-//                                     </div>
-//                                     <div style={{fontSize:"25px", fontWeight:"bold", marginLeft:"80px", marginTop:"80px", textAlign:"left"}}>
-//                                         KREAM 웹 사이트 클론 프로젝트<br></br><label style={{fontWeight:"lighter"}}>2024.01.31 ~ 2024.08.02</label>
-//                                         <p style={{fontWeight:"lighter"}}>sdasdasd</p>
-//                                     </div>
-//                                 </div>
-//                         </div>
-
-
-//                         <div style={{backgroundColor:'#ffffff',width:'1280px',height:'550px',boxShadow:'1rem 1rem 1rem 1rem rgb(68 68 68 / 20%)',borderRadius:'30px', marginTop:"50px"}}>
-//                         <div style={{display:"flex"}}>
-//                                     <div style={{textAlign:"left", marginLeft:"20px",marginTop:'20px'}}>
-//                                         <img style={{width:'550px',height:'400px', marginTop:"50px",marginLeft:'60px',border:'2px solid black',borderRadius:'10px'}} src={`${process.env.PUBLIC_URL}/img/project/project1.png`} alt='itbank'></img>
-//                                     </div>
-//                                     <div style={{fontSize:"25px", fontWeight:"bold", marginLeft:"80px", marginTop:"80px", textAlign:"left"}}>
-//                                         KREAM 웹 사이트 클론 프로젝트<br></br><label style={{fontWeight:"lighter"}}>2024.01.31 ~ 2024.08.02</label>
-//                                         <p style={{fontWeight:"lighter"}}>sdasdasd</p>
-//                                     </div>
-//                                 </div>
-                                
-//                         </div>
-//                     </div>
-
-//                 </div>
-//             </div>
-//         </>
-//     )
-// });
-
-// export default Project;
 const Project = forwardRef((props, ref) => {
     return (
         <div ref={ref} className="project-container">
@@ -56,30 +11,29 @@ const Project = forwardRef((props, ref) => {
 
                 <div className="project-card">
                     <div className="project-card-img">
-                        <video className='project1-video' src={`${process.env.PUBLIC_URL}/video/coin_investment.mp4`} alt='coin_investment' controls/>
-                        {/* <img src={`${process.env.PUBLIC_URL}/img/project/project1.png`} alt='project1' /> */}
+                        <video className='project-video' src={`${process.env.PUBLIC_URL}/video/coin_investment.mp4`} alt='coin_investment' controls/>
                     </div>
                     <div className="project1-card-desc">
                         <p className='project-title'>COIN_INVESTMENT</p>
                         <p className='project-period'>2024.04.11 ~ 2024.04.26</p>
                         <p className='project-detail'>
                             Upbit 사이트에서 제공되는 코인의 가격을 기준으로 사용자가<br/>
-                            코인을 가상투자 할 수 있는 사이트를 구현하였습니다.<br/><br/>
+                            코인을 가상투자 할 수 있는 사이트를 구현하였습니다.<br/>
+                            <br/>
                             프로젝트는 node.js를 사용하여 구현되었습니다.
                         </p>
                         <p className='project-url-text'>GitHub 코드 : </p>
                         <a className='project-address' href='https://github.com/youngho3358/coin_investment' >GitHub Repository로 이동하기</a>
                         <br/>
                         <br/>
-                        <p className='project-url-text'>자세한 포트폴리오 내용 (p.5 ~ p.16) : </p>
-                        <a className='project-address' href='https://docs.google.com/presentation/d/15SMR5FeC9Yr3w2w6wLuptSZDr3obwAIbMujhV3JVEro/#slide=id.g2f211914f85_0_3' >포트폴리오로 보러가기</a>
+                        <p className='project-url-text'>자세한 포트폴리오 내용</p>
+                        <a className='project-address' href='https://docs.google.com/presentation/d/1ON_78aC0QKPjjZIxxK3ApBAt1zkyDrKiNnrYdJNBsUs/edit?usp=sharing' >포트폴리오로 보러가기</a>
                     </div>
                 </div>
 
                 <div className="project-card">
                     <div className="project-card-img">
-                        <video className='project1-video' src={`${process.env.PUBLIC_URL}/video/coin_investment.mp4`} alt='coin_investment' controls/>
-                        {/* <img src={`${process.env.PUBLIC_URL}/img/project/project1.png`} alt='project1' /> */}
+                        <video className='project-video' src={`${process.env.PUBLIC_URL}/video/muzinut.mp4`} alt='muzinut' controls/>
                     </div>
                     <div className="project2-card-desc">
                         <p className='project-title'>Muzinut</p>
@@ -87,9 +41,55 @@ const Project = forwardRef((props, ref) => {
                         <p className='project-detail'>
                             뮤지션들이 자유롭게 자신의 음악 작업물을 게시하고<br/>
                             게시판을 통해 팬들과 소통할 수 있으며 리스너들은 게시된<br/>
-                            음원을 스트리밍하여 들을 수 있는 사이트를 구현하였습니다.<br/><br/>
+                            음원을 스트리밍하여 들을 수 있는 사이트를 구현하였습니다.<br/>
+                            <br/>
                             프로젝트는 Spring Framework 와 Next.js를 사용하여<br/>
                             구현되었습니다.
+                        </p>
+                        <p className='project-url-text'>GitHub 코드 : </p>
+                        <a className='project-address' href='https://github.com/youngho3358/Muzinut' >GitHub Repository로 이동하기</a>
+                        <br/>
+                        <br/>
+                        <p className='project-url-text'>자세한 포트폴리오 내용</p>
+                        <a className='project-address' href='https://docs.google.com/presentation/d/1spGTYBFq0-sg8WIca2C1dYYSpg3QIzLhJNCzWgcpjFI/edit?usp=sharing' >포트폴리오로 보러가기</a>
+                    </div>
+                </div>
+
+                <div className="project-card">
+                    <div className="project-card-img">
+                        {/* <video className='project-video' src={`${process.env.PUBLIC_URL}/video/muzinut.mp4`} alt='portfolio' controls/> */}
+                        <div className='project-video' style={{width:'694.87px', height:'230px', textAlign:'center', paddingTop:'220px', fontSize:'25px', fontWeight:'bold'}}> - 구현 중 -</div>
+                    </div>
+                    <div className="project3-card-desc">
+                        <p className='project-title'>Portfolio</p>
+                        <p className='project-period'>2024.08.10 ~ 2024.08.30</p>
+                        <p className='project-detail'>
+                            직접 구현한 프로젝트들을 정리하여 포트폴리오 사이트를<br/>
+                            React.js 를 기반으로 구현하였습니다.<br/>
+                            <br/>
+                            리액트를 사용하여 코드를 작성한 뒤 Github 에서 제공하는<br/>
+                            Github Page 에 업로드하여 배포하였습니다.<br/>
+                        </p>
+                        <p className='project-url-text'>GitHub 코드 : </p>
+                        <a className='project-address' href='https://github.com/youngho3358/Muzinut' >GitHub Repository로 이동하기</a>
+                    </div>
+                </div>
+
+                <div className="project-card">
+                    <div className="project-card-img">
+                        {/* <video className='project1-video' src={`${process.env.PUBLIC_URL}/video/muzinut.mp4`} alt='muzinut' controls/> */}
+                        <div className='project-video' style={{width:'694.87px', height:'230px', textAlign:'center', paddingTop:'220px', fontSize:'25px', fontWeight:'bold'}}> - 구현 중 -</div>
+                    </div>
+                    <div className="project2-card-desc">
+                        <p className='project-title' style={{fontSize:'30px', marginBottom:'0px'}}>COIN_INVESTMENT</p>
+                        <p className='project-title' style={{fontSize:'30px', marginTop:'0px'}}>Backend Code Migration</p>
+                        <p className='project-period'>2024.06.20 ~ now</p>
+                        <p className='project-detail'>
+                            기존에 javascript 로 작성되었던 코드를 백엔드 코드와<br/>
+                            프론트 엔트 코드로 분리하여 코드 마이그레이션을 진행중입니다.<br/>
+                            <br/>
+                            단순히 코드의 언어만 변경하는 것이 아닌 사이트의 자체의<br/>
+                            기능 추가 구현 및 CI/CD 를 목표로 진행중인 프로젝트입니다.<br/>
                         </p>
                         <p className='project-url-text'>GitHub 코드 : </p>
                         <a className='project-address' href='https://github.com/youngho3358/Muzinut' >GitHub Repository로 이동하기</a>
@@ -100,7 +100,7 @@ const Project = forwardRef((props, ref) => {
                     </div>
                 </div>
                 
-                
+
             </div>
         </div>
     );
